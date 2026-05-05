1876170d6e823c5810618ea36035dd6e6cf311dd
maxtindall <max@maxis.fit>
2026-05-05 09:59:19 -0400

    v0.0.1 ; minor changes to UI & server.js

--- server.js
+++ server.js
@@ -15,31 +15,23 @@ io.on("connection", (socket) => {
   socket.on("join", (code) => {
     if (!rooms[code]) rooms[code] = [];
 
-    // limit to 2 users
     if (rooms[code].length >= 2) {
       socket.emit("full");
       return;
     }
 
-    // add user to room
     rooms[code].push(socket.id);
     socket.join(code);
     socket.code = code;
 
-    // assign user number (first = 1, second = 2)
     const userNumber = rooms[code].length;
 
-    // send number to this user
     socket.emit("assigned", { userNumber });
 
-    // if 2 users, start connection
     if (rooms[code].length === 2) {
       const [a, b] = rooms[code];
 
-      // first user = initiator (#1)
       io.to(a).emit("peer", { initiator: true });
-
-      // second user = receiver (#2)
       io.to(b).emit("peer", { initiator: false });
     }
   });
@@ -52,13 +44,10 @@ io.on("connection", (socket) => {
     const code = socket.code;
     if (!code || !rooms[code]) return;
 
-    // notify peer (optional, but clean)
     socket.to(code).emit("peer_left");
 
-    // remove user
     rooms[code] = rooms[code].filter(id => id !== socket.id);
 
-    // cleanup empty room
     if (rooms[code].length === 0) {
       delete rooms[code];
     }
