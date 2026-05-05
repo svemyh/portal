54d8573d2b07411913823a20b7d77d9a2fcc2a09
maxtindall <max@maxis.fit>
2026-05-05 10:40:42 -0400

    v0.0.1 ; minor changes to UI & server.js - fixes an issue where the connection failed due to server assignment

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+cconst express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 
@@ -8,28 +8,22 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
-// rooms now store structured data
 const rooms = {};
 
 io.on("connection", (socket) => {
 
   socket.on("join", (code) => {
-    // initialize room
     if (!rooms[code]) {
-      rooms[code] = {
-        users: []
-      };
+      rooms[code] = { users: [] };
     }
 
     const room = rooms[code];
 
-    // limit to 2 users
     if (room.users.length >= 2) {
       socket.emit("full");
       return;
     }
 
-    // assign user number deterministically
     const userNumber = room.users.length === 0 ? 1 : 2;
 
     room.users.push(socket.id);
@@ -37,40 +31,30 @@ io.on("connection", (socket) => {
     socket.join(code);
     socket.code = code;
 
-    // send user identity
     socket.emit("assigned", { userNumber });
 
-    // when both users present, assign roles explicitly
     if (room.users.length === 2) {
       const [first, second] = room.users;
 
-      // first = initiator (#1)
       io.to(first).emit("peer", { initiator: true });
-
-      // second = receiver (#2)
       io.to(second).emit("peer", { initiator: false });
     }
   });
 
-  // relay signaling
   socket.on("signal", ({ code, data }) => {
     socket.to(code).emit("signal", data);
   });
 
-  // cleanup on disconnect
   socket.on("disconnect", () => {
     const code = socket.code;
     if (!code || !rooms[code]) return;
 
     const room = rooms[code];
 
-    // remove user from room
     room.users = room.users.filter(id => id !== socket.id);
 
-    // notify remaining peer
     socket.to(code).emit("peer_left");
 
-    // delete empty room
     if (room.users.length === 0) {
       delete rooms[code];
     }
